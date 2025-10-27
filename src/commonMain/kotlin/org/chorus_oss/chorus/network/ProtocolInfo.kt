dfee50ccd3547ff25f20fce45ca41c349ff8c75f
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2025-10-27 20:13:49 +1300

    update version info

--- src/commonMain/kotlin/org/chorus_oss/chorus/network/ProtocolInfo.kt
+++ src/commonMain/kotlin/org/chorus_oss/chorus/network/ProtocolInfo.kt
@@ -3,9 +3,9 @@ package org.chorus_oss.chorus.network
 import org.chorus_oss.chorus.utils.SemVersion
 
 object ProtocolInfo {
-    const val VERSION = "1.21.100"
+    const val VERSION = "1.21.111"
 
-    val GAME_VERSION = SemVersion(1, 21, 10, 0, 0)
+    val GAME_VERSION = SemVersion(1, 21, 11, 1, 0)
 
     val BLOCK_STATE_VERSION = (GAME_VERSION.major shl 24) or (GAME_VERSION.minor shl 16) or (GAME_VERSION.patch shl 8)
 }
